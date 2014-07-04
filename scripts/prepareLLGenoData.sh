d3a18c8d0f306c202fbcb3eb239a16308e9309a2
RoanKanninga <roankanninga@gmail.com>
2014-07-04 13:21:33 +0200

    removed the pseudo part in the home folder of Renier (data is now directly put on prm by datamanagers)

--- scripts/prepareLLGenoData.sh
+++ scripts/prepareLLGenoData.sh
@@ -1,43 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 set -u
 set -e
 
-module load jdk
-
-if [[ "$(hostname)" != "scheduler02" ]];
-then 
-	echo "You should be on scheduler02 to run this script"
-fi
-if [  ${1-:} ];
-then
-	echo "
-	Required:
-        -r|--runid		Put here the runnumber
-                        	e.g. OV10-0051
-
-        -d|--data		Give the type of Data (Unimputed,1000G,GONL,HapMap2)
-
-	Optional:
-	-p|--pseudodir		Put here the absolute path of the directory of the pseudofile
-				pseudo example name: OV10-0051.txt
-				default: /gcc/groups/lifelines/home/rsnieders/GenoPseudo
-									
-	" 		 	
-	 
-else
- 
-
-PARSED_OPTIONS=$(getopt -n "$0"  -o r:d:p: --long "runid:,data:,pseudodir:"  -- "$@")
-
-#Bad arguments, something has gone wrong with the getopt command.
-if [ $? -ne 0 ];
-then
-  exit 1
-fi
-
-eval set -- "$PARSED_OPTIONS"
-
+#
+# Defaults
+#
 RunId=""
 Data=""
 PseudoDir="/gcc/groups/lifelines/home/rsnieders/GenoPseudo"
@@ -45,8 +13,51 @@ GeneratedScript="/gcc/groups/lifelines/tmp03/lifelines/generatedscripts/${RunId}
 Compute5="/gcc/groups/lifelines/tmp03/lifelines/tools/molgenis_compute5/molgenis-compute-core-0.0.1-SNAPSHOT-20130920/"
 SampleSheet="/gcc/groups/lifelines/prm02/samplesheets/"
 
+#
+# Load tools into environment.
+#
+module load jdk
+
+#
+# Check if we are on the right UI to generate and submit LifeLines jobs.
+#
+if [[ "$(hostname)" != "scheduler02" ]]; then 
+	echo "FATAL: You should be on scheduler02 to run this script."
+	exit 1
+fi
+
+function usage () {
+	echo "
+	Required:
+        -r|--runid		Put here the runnumber
+                        	e.g. OV10_0051
+
+        -d|--data		Give the type of Data (Unimputed,1000G,GONL or HapMap2)
+
+	Optional:
+	-p|--pseudodir		Absolute path of the directory of the pseudofile
+				Pseudo file name must be: \${runid}.txt
+				Pseudo files are usually located in /gcc/groups/lifelines/home/\${datamanager}/GenoPseudo/
+	"
+}
+
+PARSED_OPTIONS=$(getopt -n "$0"  -o r:d:p: --long "runid:,data:,pseudodir:"  -- "$@")
+
+#
+# Bad arguments, something has gone wrong with the getopt command.
+#
+if [ $? -ne 0 ]; then
+	usage
+	echo "FATAL: Wrong arguments."
+	exit 1
+fi
+
+eval set -- "$PARSED_OPTIONS"
+
+#
 # Now goes through all the options with a case and using shift to analyse 1 argument at a time.
-#$1 identifies the first argument, and when we use shift we discard the first argument, so $2 becomes $1 and goes again through the case.
+# $1 identifies the first argument, and when we use shift we discard the first argument, so $2 becomes $1 and goes again through the case.
+#
 while true; do
   case "$1" in
 	-p|--pseudodir)
@@ -67,85 +78,89 @@ while true; do
   esac
 done
 
-
-echo ${PseudoDir}
-echo ${RunId}
-echo ${Data}
-
-if [[ "${Data}" == "Unimputed" || "${Data}" == "HapMap2" || "${Data}" == "GONL" || "${Data}" == "1000G" ]];
-then 
-	echo "Data is okay."
-else
-       echo "please fill in 'Unimputed', 'HapMap2', 'GONL' or '1000G'" 
-fi 
-
-
-#go to directory to get the Geno Data
-
-cd ${PseudoDir}
-
-
-#copy the mapping file tmp03
-if [ -f ${RunId}.txt ];
-	then
-	cp ${RunId}.txt /gcc/groups/lifelines/tmp03/lifelines/pseudoFiles 
-else
-	echo "file does not exist"
+#
+# Check required options were provided. 
+#
+if [[ -z "${RunId-}" || -z "${Data-}" ]]; then
+	usage
+        echo "FATAL: missing required parameter."
+	exit 1
 fi
 
-#go to samplesheets on permanent storage
-cd ${SampleSheet}
+if [[ "${Data}" == "Unimputed" || "${Data}" == "HapMap2" || "${Data}" == "GONL" || "${Data}" == "1000G" ]]; then 
+	echo "Requested data type is: ${Data}."
+else
+	usage
+	echo "FATAL: Unkown data type. Please choose one of 'Unimputed', 'HapMap2', 'GONL' or '1000G'"
+	exit 1
+fi 
 
-#copy an already existing old run mapping file 
-cp example_${Data}.csv ${RunId}_${Data}.csv
+echo "INFO: Using pseudodir ${PseudoDir}."
+echo "INFO: Using runid     ${RunId}."
+echo "INFO: Using data      ${Data}."
 
-#rename the old run number in the file
-perl -pi -e "s|example|${RunId}|g" ${RunId}_${Data}.csv
+#
+# Go to samplesheets on permanent storage.
+#
+cd "${SampleSheet}"
 
+#
+# Copy an already existing old run mapping file.
+#
+cp "example_${Data}.csv" "${RunId}_${Data}.csv"
 
+#
+# Rename the old run number in the file.
+#
+perl -pi -e "s|example|${RunId}|g" "${RunId}_${Data}.csv"
 
-
-#make directory in generatedscripts and load jdk
-if [ ! -e ${GeneratedScript} ];
-then
+#
+# Make directory in generatedscripts.
+#
+if [ ! -e "${GeneratedScript}" ]; then
 	mkdir ${GeneratedScript}
 	echo "created folder: ${GeneratedScript}"
 	cd ${GeneratedScript}
 	mkdir ${Data}
-elif [[ -d ${GeneratedScript} && -r ${GeneratedScript} && -w ${GeneratedScript} && -x ${GeneratedScript}  ]]; then
-	echo "directory already exists, and it is r/w/x" 
-	cd ${GeneratedScript}
-	if [ ! -d ${Data} ];
-	then
-		mkdir ${Data}
+elif [[ -d "${GeneratedScript}" && -r "${GeneratedScript}" && -w "${GeneratedScript}" && -x "${GeneratedScript}" ]]; then
+	echo "INFO: Directory already exists, and we have rwx permissions."
+	cd "${GeneratedScript}"
+	if [ ! -d "${Data}" ]; then
+		mkdir -p "${Data}"
 	fi
-	
 else
-	echo "error, the ${GeneratedScript} already exists, but you don't have read, write or executable rights"
+	echo "FATAL: The ${GeneratedScript} folder already exists, but you don't have read, write or execute rights."
+	exit 1
 fi
 
 WORKFLOW=""
 
-if [ ${Data} == "Unimputed" ];
-then 
+if [ ${Data} == "Unimputed" ]; then
 	WORKFLOW="workflowUnimputedData.csv"
 else
 	WORKFLOW="workflow.csv"
 fi
 
-#generate code
+#
+# Generate jobs.
+#
 sh ${Compute5}/molgenis_compute.sh -b pbs -p ${SampleSheet}/${RunId}_${Data}.csv \
--p ${Compute5}/pipelines/LifeLines_update_genotype_Data/parameters.csv \
--w ${Compute5}/pipelines/LifeLines_update_genotype_Data/${WORKFLOW} \
+-p ${Compute5}/pipelines/LifeLines_update_genotype_data/parameters.csv \
+-w ${Compute5}/pipelines/LifeLines_update_genotype_data/${WORKFLOW} \
 -rundir ${GeneratedScript}/${Data}/
 
-#go to the generatedscripts folder 
-cd ${GeneratedScript}/${Data}/
+#
+# Go to the generatedscripts folder.
+#
+cd "${GeneratedScript}/${Data}/"
 
-#now change the queue from ‘PBS’ to ‘devel’ since you are on scheduler02
+#
+# Now change the queue from ‘PBS’ to ‘devel’ since you are on scheduler02.
+#
 perl -pi -e 's/#PBS -q gaf/#PBS -q devel/g' *.sh
 
-#be sure that you are the lifelines group
-newgrp lifelines
-
-fi
+#
+# Signal success.
+#
+echo "INFO: Finished generating jobs, you may now run ${GeneratedScript}/${Data}/submit.sh..."
+exit 0
