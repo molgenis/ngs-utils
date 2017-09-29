1eec269ac9f7748d8c7f6534d92386e80cb3d302
BenjaminsM <m.benjamins@umcg.nl>
2017-09-29 11:44:54 +0200

    Added making symlinks for new Bedfiles + getopts

--- makeBedForDiagnostics.sh
+++ makeBedForDiagnostics.sh
@@ -1,67 +1,119 @@
 set -e
 set -u
 
+function showHelp() {
+	#
+	# Display commandline help on STDOUT.
+	#
+	cat <<EOH
+===============================================================================================================
+Script to make Bed files for Diagnostics.
+Usage:
+	$(basename $0) OPTIONS
+Options:
+	-h   Show this help.
+	-b   Name of the BED file
+	-d   Name of the directory
+	-e   Making BED file for exomekit [default=false]
+===============================================================================================================
+EOH
+	trap - EXIT
+	exit 0
+}
+
+
+while getopts "b:d:e:h" opt;
+do
+	case $opt in h)showHelp;; b)bedfile="${OPTARG}";; d)directory="${OPTARG}";; e)exome="${OPTARG}";;
+	esac
+done
+
+if [[ -z "${bedfile:-}" ]]
+then
+	echo -e '\nERROR: Must specify a BED file!\n'
+
+	showHelp
+        exit 1
+fi
+
+
+if [[ -z "${directory:-}" ]]
+then
+	echo -e '\nERROR: Must specify a directory!\n'
+
+        showHelp
+        exit 1
+fi
+
+if [[ -z "${exome:-}" ]]
+then
+	exome="false"
+else
+	exome="true"
+fi
+
+
+if [ -d /apps/data/Agilent/$directory ]
+then
+	echo "/apps/data/Agilent/$directory already exists"
+	exit 1
+elif [ -d /apps/data/UMCG/Diagnostics/$directory ]
+then
+	echo "/apps/data/UMCG/Diagnostics/$directory already exists"
+	exit 1
+fi
+
 thisDir=$(pwd)
+UMCGDir=/apps/data/UMCG/Diagnostics/
 
-if [ $thisDir != "/apps/data/Agilent" ]
-then
-	echo "you should be in /apps/data/Agilent to run this"
-	exit 1
-
-fi
-
-fileName=$1
-if [ -z  ${1+x} ]
-then
-	echo "expecting 2 arguments (filename and newFile name)"
-	echo "e.g. sh makeBedForDiagnostics.sh PCS_3004471_+en-20_target_v2.BED PCS_v4"
-	exit 1
-fi
+mkdir -p ${directory}/human_g1k_v37/
+echo "created ${directory}/human_g1k_v37/"
+cp ${bedfile} ${directory}/
+echo "copied ${bedfile} ${directory}/"
+## navigate to folder
+cd ${directory}
 
 
-if [[ $fileName != *target* ]]
-then
-	echo "expecting target file, not bp.. If it is the target file, than please put in the name"
-	exit 1
-fi	
+cp ${bedfile} human_g1k_v37/captured.bed
+echo "copied ${bedfile} to human_g1k_v37/captured.bed"
 
-newName=$2
-
-if [ -d /apps/data/Agilent/$newName ]
-then
-	echo "/apps/data/Agilent/$newName already exists"
-	exit 1
-elif [ -d /apps/data/UMCG/Diagnostics/$newName ]
-then
-	echo "/apps/data/UMCG/Diagnostics/$newName already exists"
-	exit 1
-fi
-
-mkdir -p ${newName}/human_g1k_v37/
-echo "created ${newName}/human_g1k_v37/"
-cp ${fileName} ${newName}/
-echo "copied ${fileName} ${newName}/"
-## navigate to folder 
-cd ${newName}
-
-
-cp ${fileName} human_g1k_v37/captured.bed
-echo "copied ${fileName} to human_g1k_v37/captured.bed"
 module load ngs-utils
 
 cd human_g1k_v37/
 
 ## Run the prepare step
-sh ${EBROOTNGSMINUTILS}/prepare_NGS_Bedfiles.sh -n captured -c true -d targeted
 
-## 
+if [[ ${exome} == 'true' ]]
+	then
+	echo 'Creating Bedfiles for a New ExomeKit'
+	sh ${EBROOTNGSMINUTILS}/prepare_NGS_Bedfiles.sh -n captured
+else
+	echo "Creating Bedfiles for a New kit ${directory}"
+	sh ${EBROOTNGSMINUTILS}/prepare_NGS_Bedfiles.sh -n captured -c true -d targeted
+fi
+
+##
 cd $thisDir
-echo "copied ${newName} to /apps/data/UMCG/Diagnostics/"
-cp -r ${newName} /apps/data/UMCG/Diagnostics/
+echo "copied ${directory} to ${UMCGDir}"
+cp -r ${directory} ${UMCGDir}
 
-cd /apps/data/UMCG/Diagnostics/${newName}/human_g1k_v37/
-echo "renaming captured into ${newName}"
-rename captured ${newName} captured.*
+cd ${UMCGDir}/${directory}/human_g1k_v37/
+echo "renaming captured into ${directory}"
+rename captured ${directory} captured.*
+
+
+
+#perbase
+cd ${UMCGDir}/CoveragePerBase/
+mkdir ${directory}
+cd ${directory}
+ln -sf ../../${directory}/
+
+#pertarget
+cd ${UMCGDir}/CoveragePerTarget/
+mkdir ${directory}
+cd ${directory}
+ln -sf ../../${directory}/
 
 echo "FINISHED"
 
