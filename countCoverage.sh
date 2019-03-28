6afa9a834030d78c9210060dca5d5e1b9cc737d9
RoanKanninga <roankanninga@gmail.com>
2019-03-28 08:24:47 +0100

    updated the countCoverage scripts

--- countCoverage.sh
+++ countCoverage.sh
@@ -32,7 +32,7 @@ weaved command: ls /groups/umcg-gd/prm02/projects/*-Exoom_v1/run01/results/cover
 }
 
 module load ngs-utils
-PARSED_OPTIONS=$(getopt -n "$0"  -o p:w:s:d:t: --long "name:panel:workdir:structure:permanentdir:tmp:"  -- "$@")
+PARSED_OPTIONS=$(getopt -n "$0"  -o p:w:s:d:t: --long "panel:workdir:structure:permanentdir:tmp:"  -- "$@")
 
 #
 # Bad arguments, something has gone wrong with the getopt command.
@@ -58,7 +58,7 @@ while true; do
             esac ;;
 	-w|--workdir)
                 case "$2" in
-                *) WORDKIR=$2 ; shift 2 ;;
+                *) WORKDIR=$2 ; shift 2 ;;
             esac ;;
 	-s|--structure)
                 case "$2" in
@@ -95,24 +95,23 @@ if [[ -z "${PRMDIR-}" ]]; then
 fi
 if [[ -z "${TMP-}" ]]; then
 	TMP="${WORKDIR}/tmp/"
-	mkdir -p "${TMP}"
-	echo "makedir ${TMP}"
 fi
 echo "starting, first cleaning up old run in ${WORKDIR}"
 
 rm -rf "${WORKDIR}"
 mkdir -p "${WORKDIR}/coverage/"
-mkdir -p "${WORKDIR}/tmp/"
+mkdir -p "${TMP}"
 
 echo "starting to extract all the ${PANEL} projects and retreive the coverage for each sample"
-echo "ls ${PRMDIR}/*${PANEL}/${STRUCTURE}/*${PANEL}*.coveragePerTarget.txt"
+#echo "ls ${PRMDIR}/*${PANEL}/${STRUCTURE}/*${PANEL}*.coveragePerTarget.txt"
 count=0
 SAMPLES=()
 REJECTEDSAMPLES=()
-
-if find ${PRMDIR}/*${PANEL}/${STRUCTURE}/*${PANEL}*.coveragePerTarget.txt -type f -mtime -120 -exec ls -la  {} \;
+#if find /groups/umcg-gd/tmp06/testCoverage/*${PANEL}*.coveragePerTarget.txt -type f -mtime -120 -exec ls -la  {} \;
+if find ${PRMDIR}/*${PANEL}/${STRUCTURE}/*${PANEL}*.coveragePerTarget.txt -type f -mtime -12 -exec ls -la  {} \;
 then
-	for i in $(find ${PRMDIR}/*${PANEL}/${STRUCTURE}/*${PANEL}*.coveragePerTarget.txt -type f -mtime -120 )
+	#for i in $(find /groups/umcg-gd/tmp06/testCoverage/*${PANEL}*.coveragePerTarget.txt -type f -mtime -120 )
+	for i in $(find ${PRMDIR}/*${PANEL}/${STRUCTURE}/*${PANEL}*.coveragePerTarget.txt -type f -mtime -12 )
 	do
 		sampleName="$(basename "${i%%.*}")"
 		SAMPLES+=("${sampleName}")
@@ -121,7 +120,6 @@ then
 			awk '{print $2,$3,$4,$6}' "${i}" > "${TMP}/firstcolumns.txt"
 			count=$((count+1))
 		fi
-
 		NAMEFILE=$(basename "${i}")
 		DIRNAME=$(dirname "${i}")
 		SAMPLENAME=${NAMEFILE%%.*}
@@ -160,72 +158,74 @@ then
 	done
 fi
 
-echo ${SAMPLES[@]} > ${TMP}/headers.txt"
+echo "${SAMPLES[@]}" > "${TMP}/headers.txt"
 
 echo "${WORKDIR}/coverage/"
 
 total=$(ls "${WORKDIR}/coverage/"*".coverage" | wc -l)
 
-paste "${WORKDIR}/coverage/"*".coverage" > "${TMP}/coverageAllSamples.txt"
+paste "${WORKDIR}/coverage/"*".coverage" > "${TMP}/coverageAllSamples.txt.tmp"
 
 
-echo "## calculate MEDIAN ##"
-## calculate MEDIAN ##
-awk -v max=1 '
-        function median(c,v,  j) { 
-           asort(v,j); 
-           if (c % 2) return j[(c+1)/2]; 
-           else return (j[c/2+1]+j[c/2])/2.0; 
-        } 
-{ 
-         count++;values[count]=$NF;  
-         if (count >= max) { 
-           print  median(count,values); count=0; 
-         } 
-} 
-END { 
-         print  median(count,values); 
-}' "${TMP}/coverageAllSamples.txt" > "${TMP}/coverageAllSamples_Median.txt"
+## removing header line
+tail -n+2 "${TMP}/coverageAllSamples.txt.tmp" > "${TMP}/coverageAllSamples.txt"
+
 echo "## calculate SD ##" 
 ## calculate SD
 awk '{ A=0; V=0; for(N=1; N<=NF; N++) A+=$N ; A/=NF ; for(N=1; N<=NF; N++) V+=(($N-A)*($N-A))/(NF-1); print sqrt(V) }' "${TMP}/coverageAllSamples.txt" > "${TMP}/coverageAllSamples_SD.txt"
-
 echo "## calculate AVG ##"
 ## CALCULATE AVG
 awk '{ for(i = 1; i <= NF; i++) sum+=$i;print sum/NF;sum=0 }' "${TMP}/coverageAllSamples.txt" > "${TMP}/coverageAllSamples_AVG.txt"
 
 echo "## Calculate percentage under 10,20,30,50 and 100x ##"
 ## Calculate percentage under 10,20,50 and 100x ##
-awk '{ for(i = 1; i <= NF; i++) if ($i < 10 )counter+=1;print 100-((counter/NF))*100;counter=0 }'  "${TMP}/coverageAllSamples.txt" > "${TMP}/coverageAllSamples_moreThan10x.txt"
-awk '{ for(i = 1; i <= NF; i++) if ($i < 20 )counter+=1;print 100-((counter/NF))*100;counter=0 }'  "${TMP}/coverageAllSamples.txt" > "${TMP}/coverageAllSamples_moreThan20x.txt"
-awk '{ for(i = 1; i <= NF; i++) if ($i < 30 )counter+=1;print 100-((counter/NF))*100;counter=0 }'  "${TMP}/coverageAllSamples.txt" > "${TMP}/coverageAllSamples_moreThan30x.txt"
-awk '{ for(i = 1; i <= NF; i++) if ($i < 50 )counter+=1;print 100-((counter/NF))*100;counter=0 }'  "${TMP}/coverageAllSamples.txt" > "${TMP}/coverageAllSamples_moreThan50x.txt"
-awk '{ for(i = 1; i <= NF; i++) if ($i < 100 )counter+=1;print 100-((counter/NF))*100;counter=0 }'  "${TMP}/coverageAllSamples.txt" > "${TMP}/coverageAllSamples_moreThan100x.txt"
+awk '{ for(i = 1; i <= NF; i++) if ($i < 10 || $i == "NaN")counter+=1;print 100-((counter/NF))*100;counter=0 }'  "${TMP}/coverageAllSamples.txt" > "${TMP}/coverageAllSamples_moreThan10x.txt"
+awk '{ for(i = 1; i <= NF; i++) if ($i < 20 || $i == "NaN")counter+=1;print 100-((counter/NF))*100;counter=0 }'  "${TMP}/coverageAllSamples.txt" > "${TMP}/coverageAllSamples_moreThan20x.txt"
+awk '{ for(i = 1; i <= NF; i++) if ($i < 30 || $i == "NaN")counter+=1;print 100-((counter/NF))*100;counter=0 }'  "${TMP}/coverageAllSamples.txt" > "${TMP}/coverageAllSamples_moreThan30x.txt"
+awk '{ for(i = 1; i <= NF; i++) if ($i < 50 || $i == "NaN")counter+=1;print 100-((counter/NF))*100;counter=0 }'  "${TMP}/coverageAllSamples.txt" > "${TMP}/coverageAllSamples_moreThan50x.txt"
+awk '{ for(i = 1; i <= NF; i++) if ($i < 100 || $i == "NaN")counter+=1;print 100-((counter/NF))*100;counter=0 }'  "${TMP}/coverageAllSamples.txt" > "${TMP}/coverageAllSamples_moreThan100x.txt"
 
-awk 'BEGIN{OFS="\t"}{print $1,$2,$3,$4}' "/apps/data/Agilent/${PANEL}/human_g1k_v37/captured.merged.bed" > "${TMP}/first4columns.txt"
+awk 'BEGIN{OFS="\t"}{print $1,$2,$3,$4}' "/apps/data/Agilent/${PANEL}/human_g1k_v37/captured.merged.bed" > "${TMP}/first4columns.txt.tmp"
+grep -v 'phiX' "${TMP}/first4columns.txt.tmp" > "${TMP}/first4columns.txt"
 
 echo "## update column gene with only one annotation possible"
 ## update column gene with only one annotation possible
-
-
 echo "pasting median,avg,10x,20x,30x,50x and 100x"
 rm -f "${WORKDIR}/CoverageOverview.txt"
 
 firstPartOfLink="https://genome.ucsc.edu/cgi-bin/hgTracks?db=hg19&lastVirtModeType=default&lastVirtModeExtraState=&virtModeType=default&virtMode=0&nonVirtPosition=&position="
 secondPartOfLink="&hgsid=653811211_bIwQegXO9Zbd8eoOt7J1cdi7D9zi"
 
-paste -d '\t' "${TMP}/first4columns.txt" "${TMP}/coverageAllSamples_Median.txt" "${TMP}/coverageAllSamples_AVG.txt" "${TMP}/coverageAllSamples_SD.txt" "${TMP}/coverageAllSamples_moreThan10x.txt" "${TMP}/coverageAllSamples_moreThan20x.txt ${TMP}/coverageAllSamples_moreThan20x.txt ${TMP}/coverageAllSamples_moreThan50x.txt ${TMP}/coverageAllSamples_moreThan100x.txt > ${TMP}/pasteAllInfoTogether.txt"
-echo -e "Chr\tStart\tStop\tGene\tMedian\tAvgCoverage\tSD\tmoreThan10x\tmoreThan20x\tmoreThan30x\tmoreThan50x\tmoreThan100x\tgenomeBrowse" > "${WORKDIR}/CoverageOverview.txt"
-tail -n+2 "${TMP}/pasteAllInfoTogether.txt" >> "${WORKDIR}/CoverageOverview.txt"
-head -n -1 "${WORKDIR}/CoverageOverview.txt" > "${TMP}/CoverageOverview.txt.tmp"
-awk -v link1="${firstPartOfLink}" -v link2="${secondPartOfLink}" '{OFS="\t"}{OFMT="%.2f"; if (NR==1){print $0}else{print $1,$2,$3,$4,$5/1,$6/1,$7/1,$8/1,$9/1,$10/1,$11/1,$12/1,link1""$1"%3A"$2""link2}}' ${TMP}/CoverageOverview.txt.tmp > ${WORKDIR}/CoverageOverview_basedOn_${total}_Samples.txt
+cat "${TMP}/first4columns.txt" | wc -l > "${TMP}/countrows.txt"
+cat "${TMP}/coverageAllSamples_AVG.txt" | wc -l >> "${TMP}/countrows.txt"
+cat "${TMP}/coverageAllSamples_SD.txt" | wc -l >> "${TMP}/countrows.txt"
+cat "${TMP}/coverageAllSamples_moreThan10x.txt" | wc -l >> "${TMP}/countrows.txt"
+cat "${TMP}/coverageAllSamples_moreThan20x.txt" | wc -l >> "${TMP}/countrows.txt"
+cat "${TMP}/coverageAllSamples_moreThan30x.txt" | wc -l >> "${TMP}/countrows.txt"
+cat "${TMP}/coverageAllSamples_moreThan50x.txt" | wc -l >> "${TMP}/countrows.txt"
+cat "${TMP}/coverageAllSamples_moreThan100x.txt" | wc -l >> "${TMP}/countrows.txt"
+
+uniqCountRows=$(sort -V "${TMP}/countrows.txt" | uniq | wc -l)
+if [ "${uniqCountRows}" != 1 ]
+then
+	echo "${uniqCountRows}"
+	echo "error, there are files containing more rows than others, exiting"
+	echo "please check the counts of the following files in this folder ${TMP}:"
+	echo "first4columns.txt and coverageAllSamples_*.txt"
+	exit 1
+fi
+## Pasting all files together
+paste -d '\t' "${TMP}/first4columns.txt" "${TMP}/coverageAllSamples_AVG.txt" "${TMP}/coverageAllSamples_SD.txt" "${TMP}/coverageAllSamples_moreThan10x.txt" "${TMP}/coverageAllSamples_moreThan20x.txt" "${TMP}/coverageAllSamples_moreThan20x.txt" "${TMP}/coverageAllSamples_moreThan50x.txt" "${TMP}/coverageAllSamples_moreThan100x.txt" > "${TMP}/pasteAllInfoTogether.txt"
+echo -e "Chr\tStart\tStop\tGene\tAvgCoverage\tSD\tmoreThan10x\tmoreThan20x\tmoreThan30x\tmoreThan50x\tmoreThan100x\tgenomeBrowse" > "${WORKDIR}/CoverageOverview.txt"
+cat "${TMP}/pasteAllInfoTogether.txt" >> "${WORKDIR}/CoverageOverview.txt"
+awk -v link1="${firstPartOfLink}" -v link2="${secondPartOfLink}" '{OFS="\t"}{OFMT="%.2f"; if (NR==1){print $0}else{print $1,$2,$3,$4,$6/1,$7/1,$8/1,$9/1,$10/1,$11/1,$12/1,link1""$1"%3A"$2""link2}}' ${WORKDIR}/CoverageOverview.txt > ${WORKDIR}/CoverageOverview_basedOn_${total}_Samples.txt
 
 echo "DONE, final file is ${WORKDIR}/CoverageOverview_basedOn_${total}_Samples.txt"
 
 echo "splitting on comma"
-awk '{OFS="\t"}{n=split($4,A,","); for (i in A){print $1,$2,$3,A[i],$5,$6,$7,$8,$9,$10,$11,$12,$13}}' "${WORKDIR}/CoverageOverview_basedOn_${total}_Samples.txt" > ${TMP}/CoverageOverview_basedOn_${total}_Samples_splittedComma.txt
+awk '{OFS="\t"}{n=split($4,A,","); for (i in A){print $1,$2,$3,A[i],$5,$6,$7,$8,$9,$10,$11,$12}}' "${WORKDIR}/CoverageOverview_basedOn_${total}_Samples.txt" > ${TMP}/CoverageOverview_basedOn_${total}_Samples_splittedComma.txt
 echo "splitting on semi colon"
-awk '{OFS="\t"}{n=split($4,A,":"); for (i in A){print $1,$2,$3,A[i],$5,$6,$7,$8,$9,$10,$11,$12,$13}}' "${TMP}/CoverageOverview_basedOn_${total}_Samples_splittedComma.txt" > "${TMP}/CoverageOverview_basedOn_${total}_Samples_splittedCommaAndSemiColon.txt"
+awk '{OFS="\t"}{n=split($4,A,":"); for (i in A){print $1,$2,$3,A[i],$5,$6,$7,$8,$9,$10,$11,$12}}' "${TMP}/CoverageOverview_basedOn_${total}_Samples_splittedComma.txt" > "${TMP}/CoverageOverview_basedOn_${total}_Samples_splittedCommaAndSemiColon.txt"
 
 cp "${TMP}/CoverageOverview_basedOn_${total}_Samples_splittedCommaAndSemiColon.txt" "${WORKDIR}/CoverageOverview_basedOn_${total}_SamplesFinal.txt"
 
@@ -237,21 +237,39 @@ mkdir -p "${TMP}/perGene/"
 echo "writing all the targets to the genes file, this will take some time"
 awk -v tmpDirectory="${TMP}/perGene/" '{if (NR>1){print $0 > tmpDirectory$4".txt"}}' "${WORKDIR}/CoverageOverview_basedOn_${total}_SamplesFinal.txt"
 echo "genomeBrowser > ${TMP}/perGene/allGenes.startStop"
+totalNoGenes=$(ls ${TMP}/perGene/*.txt | wc -l)
+count25=$((totalNoGenes/4))
+count50=$((totalNoGenes/2))
+count75=$((count25*3))
+echo "starting to process all ${totalNoGenes} genes"
+loopcount=0
 for i in $(ls ${TMP}/perGene/*.txt)
 do
+	if [ "${loopcount}" == "${count25}" ]
+	then
+		echo "25% done"
+	elif [ "${loopcount}" == "${count50}" ]
+	then
+		echo "50% done"
+	elif [ "${loopcount}" == "${count75}" ]
+	then
+		echo "75% done"
+	fi
+
 	geneName="$(basename "${i%.txt}")"
-	echo "working on $geneName"
 	head -1 "${i}" | awk -v g="${geneName}" -v link="${firstPartOfLink}" '{print link""$1"%3A"$2}' > "${i}.start"
 	tail -1 "${i}" | awk -v link="${secondPartOfLink}" '{print $3""link}' > "${i}.stop"
 	paste -d"-" "${i}.start" "${i}.stop" >> "${TMP}/perGene/allGenes.startStop"
+	loopcount=$((loopcount +1))
 done
-
-echo "python ~/github/ngs-utils/countCoveragePerGene.py ${WORKDIR}/CoverageOverview_basedOn_${total}_SamplesFinal.txt  > ${WORKDIR}/CoverageOverview_PerGene.txt"
+total=$(ls "${WORKDIR}/coverage/"*".coverage" | wc -l)
+echo "python ${EBROOTNGSMINUTILS}/countCoveragePerGene.py ${WORKDIR}/CoverageOverview_basedOn_${total}_SamplesFinal.txt  > ${WORKDIR}/CoverageOverview_PerGene.txt"
+#python ${EBROOTNGSMINUTILS}/countCoveragePerGene.py ${WORKDIR}/CoverageOverview_basedOn_${total}_SamplesFinal.txt  > ${WORKDIR}/CoverageOverview_PerGene.txt
 python ~/github/ngs-utils/countCoveragePerGene.py ${WORKDIR}/CoverageOverview_basedOn_${total}_SamplesFinal.txt  > ${WORKDIR}/CoverageOverview_PerGene.txt
 
-awk '{OFS="\t"}{OFMT="%.2f"; print $1,$2/1,$3,$4/1,$5/1,$6/1,$7/1,$8/1,$9/1,$10/1}' ${WORKDIR}/CoverageOverview_PerGene.txt > ${WORKDIR}/CoverageOverview_PerGene.txt.tmp
+awk '{OFS="\t"}{OFMT="%.2f"; print $1,$2/1,$3,$4/1,$6/1,$7/1,$8/1,$9/1,$10/1}' ${WORKDIR}/CoverageOverview_PerGene.txt > ${WORKDIR}/CoverageOverview_PerGene.txt.tmp
 
-echo -e "Gene\tAvgCoverage\tNo_of_Targets\tMedian\tSD\tmoreThan10x\tmoreThan20x\tmoreThan30x\tmoreThan50x\tmoreThan100x" > ${TMP}/CoverageOverview_PerGene_basedOn_${total}_Samples.txt.tmp
+echo -e "Gene\tAvgCoverage\tNo_of_Targets\tSD\tmoreThan10x\tmoreThan20x\tmoreThan30x\tmoreThan50x\tmoreThan100x" > ${TMP}/CoverageOverview_PerGene_basedOn_${total}_Samples.txt.tmp
 
 sort -V -k1 "${WORKDIR}/CoverageOverview_PerGene.txt.tmp" >> "${TMP}/CoverageOverview_PerGene_basedOn_${total}_Samples.txt.tmp"
 paste -d"\t" "${TMP}/CoverageOverview_PerGene_basedOn_${total}_Samples.txt.tmp" "${TMP}/perGene/allGenes.startStop" > "${WORKDIR}/CoverageOverview_PerGene_basedOn_${total}_Samples.txt"
